810033654b8fd009a41d7f4aba4d5123f846b986
Jakub Karas <jakub.karas@mycroftmind.com>
2023-03-21 21:47:14 +0100

    processing commands added

--- pyecc.py
+++ pyecc.py
@@ -1,16 +1,19 @@
 import logging
 from commands import get_command, CommandsEnum
+from user import User
 
 if __name__ == "__main__":
     logging.getLogger(__name__)
     logging.basicConfig(format = '\n%(asctime)s %(module)s %(levelname)s: %(message)s', datefmt = '%I:%M:%S %p', level = logging.INFO)
 
     logging.getLogger().info("Hello from PyECC.")
+    user = User()
     while True:
         try:
             command = get_command()
             logging.getLogger().info(f"Command: {command}")
             if command.command == CommandsEnum.QUIT:
                 break
+            command.process(user)
         except Exception as e:
             logging.getLogger().exception(f"An error occured: {e}")
