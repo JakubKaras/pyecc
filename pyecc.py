91822227ae171e5d910e4d80a30b6c74c84d6562
Jakub Karas <jakub.karas@mycroftmind.com>
2023-03-21 20:38:20 +0100

    commands are read

--- pyecc.py
+++ pyecc.py
@@ -1,7 +1,16 @@
 import logging
+from commands import get_command, CommandsEnum
 
 if __name__ == "__main__":
     logging.getLogger(__name__)
     logging.basicConfig(format = '\n%(asctime)s %(module)s %(levelname)s: %(message)s', datefmt = '%I:%M:%S %p', level = logging.INFO)
 
     logging.getLogger().info("Hello from PyECC.")
+    while True:
+        try:
+            command = get_command()
+            logging.getLogger().info(f"Command: {command}")
+            if command.command == CommandsEnum.QUIT:
+                break
+        except Exception as e:
+            logging.getLogger().exception(f"An error occured: {e}")
