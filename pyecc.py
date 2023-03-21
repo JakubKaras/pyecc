1e05b20f7b01a0143eedd7f123db8d5bc4c9b05d
Jakub Karas <jakub.karas@mycroftmind.com>
2023-03-21 18:36:31 +0100

    initial log added

--- pyecc.py
+++ pyecc.py
@@ -3,3 +3,5 @@ import logging
 if __name__ == "__main__":
     logging.getLogger(__name__)
     logging.basicConfig(format = '\n%(asctime)s %(module)s %(levelname)s: %(message)s', datefmt = '%I:%M:%S %p', level = logging.INFO)
+
+    logging.getLogger().info("Hello from PyECC.")
