39c52e3bae9d59cedb0040a9bbe425f807e53305
Jakub Karas <jakub.karas@mycroftmind.com>
2023-03-21 21:46:32 +0100

    several commands process function was implemented

--- commands.py
+++ commands.py
@@ -1,22 +1,39 @@
 import enum
 import logging
-from dataclasses import dataclass
 from elliptic_curves import EllipticCurve
-from user_inputs import read_int
+from user import User
+
 
 class CommandsEnum(enum.Enum):
     NULL_COMMAND = None
-    QUIT = ["-q", "quit"]
-    HELP = ["-h", "help"]
-    CIPHER = ["-c", "cipher"]
-    DECIPHER = ["-d", "decipher"]
-    SHOW_CURVE = ["--show_curve", "show"]
-    SET_CURVE = ["--set_curve", "set"]
+    QUIT = ["-q", "quit", "Close interface"]
+    HELP = ["-h", "help", "Print help to console."]
+    CIPHER = ["-c", "cipher", "Cipher message."]
+    DECIPHER = ["-d", "decipher", "Decipher message."]
+    SHOW_CURVE = ["--show_curve", "show", "Plot current elliptic curve's points."]
+    SET_CURVE = ["--set_curve", "set", "Set ciphering elliptic curve."]
 
-@dataclass
+
 class Command:
-    command: CommandsEnum
-    params: EllipticCurve | None
+    def __init__(self, command: CommandsEnum, params: list | None):
+        self.command = command
+        self.params = params
+    
+    def process(self, user: User):
+        if self.command == CommandsEnum.NULL_COMMAND:
+            logging.getLogger().info(Command._help_message())
+        if self.command == CommandsEnum.SET_CURVE:
+            user.curve = EllipticCurve(self.params)
+        if self.command == CommandsEnum.HELP:
+            logging.getLogger().info(Command._help_message())
+
+    @staticmethod
+    def _help_message() -> str:
+        help_message = "The following commands are available:\nCommand:                    Function:\n"
+        for command in CommandsEnum:
+            if command.value is not None:
+                help_message = help_message + f"  {command.value[0] + ', ':<14}{command.value[1]:<14}{command.value[2]}\n"
+        return help_message
 
 
 def get_command() -> Command:
@@ -30,19 +47,19 @@ def get_command_enum(command_string: str) -> CommandsEnum:
     for command_enum in CommandsEnum:
         if command_enum is CommandsEnum.NULL_COMMAND:
             continue
-        for command_key in command_enum.value:
+        for command_key in command_enum.value[:2]:
             if command_key in command_string:
                 return command_enum
     return CommandsEnum.NULL_COMMAND
 
-def get_command_params(command_enum: CommandsEnum) -> EllipticCurve | None:
+def get_command_params(command_enum: CommandsEnum) -> list | None:
     if command_enum == CommandsEnum.CIPHER:
         return None
     if command_enum == CommandsEnum.DECIPHER:
         return None
     if command_enum == CommandsEnum.SET_CURVE:
         logging.getLogger().info("Setting elliptic-curve in the form y^2 = x^3 + ax + b.")
-        param_a = int(read_int("Parameter a: "))
-        param_b = int(read_int("Parameter b: "))
-        return EllipticCurve(param_a, param_b)
+        param_a = int(User.read_int("Parameter a: "))
+        param_b = int(User.read_int("Parameter b: "))
+        return [param_a, param_b]
     return None
